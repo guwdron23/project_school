8c81cc132242e379957bf6763ca5267955948b02
Mukhambettt <alimuhambet38@mail.ru>
2022-12-09 13:28:40 +0600

    ali added student

--- school/settings.py
+++ school/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'teachers.apps.TeachersConfig'
+    'teachers.apps.TeachersConfig',
+    'student.apps.StudentConfig',
 ]
 
 MIDDLEWARE = [
---
+++ student/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+from .utils import validate_phone_number, validate_klas
+
+class Student(models.Model):
+    fio = models.CharField(max_length=255, verbose_name='ФИО')
+    klass = models.PositiveIntegerField(verbose_name="Класс", validators=[validate_klas])
+    phone_parent = models.CharField(max_length=55, unique=True,
+                             validators=[validate_phone_number],
+                             verbose_name='Номер Родителей',
+                             help_text='Номер должен содержать междкнародный формат')
+    live = models.CharField(max_length=255, verbose_name="Место проживания")
+    image = models.ImageField(upload_to='news/', verbose_name="Изоброжение",blank=True,null=True)
